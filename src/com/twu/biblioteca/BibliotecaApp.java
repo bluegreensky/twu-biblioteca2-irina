b179ab531fe5b99d4e63c51f27c69991a3fdc791
irina <irina.capatina.v@gmail.com>
2020-02-10 11:21:07 -0600

    1.4 / View a main menu of options.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,28 @@
 package com.twu.biblioteca;
-import com.twu.biblioteca.domain.Book;
 import com.twu.biblioteca.domain.Library;
-
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BibliotecaApp {
+
     public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            do {
+                System.out.println("Main menu");
+                System.out.println("1. List of books");
+                System.out.print("Please select an option: ");
+                String response = scanner.nextLine();
+
+                switch (response) {
+                    case "1": listBooks(); break;
+                    default: System.exit(0);
+                }
+
+            } while (true);
+        }
+    }
+
+    private static void listBooks() {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
         System.out.println(new Library());
     }
