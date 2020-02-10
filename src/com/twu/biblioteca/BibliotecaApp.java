ad3c4852e338c514b8bd0f7f5ee445d44ef394e0
irina <irina.capatina.v@gmail.com>
2020-02-10 12:04:19 -0600

    1.6 / Quit the application.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -10,17 +10,18 @@ public class BibliotecaApp {
             do {
                 System.out.println("Main menu");
                 System.out.println("1. List of books");
+                System.out.println("2. Quit the application");
                 System.out.print("Please select an option: ");
                 String response = scanner.nextLine();
 
                 switch (response) {
                     case "1": listBooks(); break;
+                    case "2": System.exit(0);
                     default:    try{
                                  notifyInvalidOption();
                                 } catch (RuntimeException e) {
                                   System.out.println(e.getMessage());
                                 }
-                                System.exit(0);
                 }
 
             } while (true);
